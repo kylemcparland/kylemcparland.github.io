c8aabe3176a48569bd46e713437341be1be0e656
Kyle McParland <kylemcparland@gmail.com>
2024-11-18 18:09:35 -0500

    Update CSS and default HEADER sizing.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/Header.css";
 import useMousePosition from "../helpers/useMousePosition";
 import useWindowWidth from "../helpers/useWindowWidth";
@@ -10,16 +10,34 @@ const Header = () => {
   //MIN-MAX RATIO CONVERSION:
   // new_value = ( (old_value - old_min) / (old_max - old_min) ) * (new_max - new_min) + new_min
 
+  // Default HEADER size...
+  const defaultScaleX = 1.2;
+  const defaultSkewX = 0;
+  const defaultFontWeight = 800;
+
   // Convert horizontal mouse position into scaleX...
-  const horizontalMove = (mousePosition.x / window.innerWidth) * 100;
-  const scaleX = ((horizontalMove - 0) / (100 - 0)) * (1.9 - 0.3) + 0.3;
+  const horizontalMove =
+    mousePosition.x !== null ? (mousePosition.x / window.innerWidth) * 100 : 50;
+  const scaleX =
+    mousePosition.x !== null
+      ? ((horizontalMove - 0) / (100 - 0)) * (1.9 - 0.3) + 0.3
+      : defaultScaleX;
 
   // Convert vertical mouse position into skewX value...
-  const verticalMove = (mousePosition.y / window.innerHeight) * 100;
-  const skewX = ((verticalMove - 0) / (100 - 0)) * (12 - -12) + -10;
+  const verticalMove =
+    mousePosition.y !== null
+      ? (mousePosition.y / window.innerHeight) * 100
+      : 50;
+  const skewX =
+    mousePosition.y !== null
+      ? ((verticalMove - 0) / (100 - 0)) * (12 - -12) + -10
+      : defaultSkewX;
 
   // Convert horizontal mouse position into fontWeight value...
-  const fontWeight = ((horizontalMove - 0) / (100 - 0)) * (900 - 700) + 700;
+  const fontWeight =
+    mousePosition.x !== null
+      ? ((horizontalMove - 0) / (100 - 0)) * (900 - 700) + 700
+      : defaultFontWeight;
 
   // Store updated values to pass to Hero-text...
   const transform = {
