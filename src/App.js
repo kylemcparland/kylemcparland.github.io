e9aab09d1916b6e8b659eaa5d17e9472963d3a7a
Kyle McParland <kylemcparland@gmail.com>
2025-01-18 17:27:04 -0500

    Add new project EVERSTRIDE to projects.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useRef } from "react";
 import "./styles/App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import MobileHeader from "./components/MobileHeader";
-import Marquee from "./components/Marquee";
+// import MobileHeader from "./components/MobileHeader";
+// import Marquee from "./components/Marquee";
 import Article from "./components/Article";
 import Project from "./components/Project";
 import ParallaxText from "./components/ParallaxText";
@@ -19,7 +19,16 @@ import {
 
 // TWEETER:
 import { Tweeter, tweeterLink, tweeterPhotos } from "./projects/Tweeter";
+
+// VHS DREAMS:
 import { VHSDreams, vhsLink, vhsPhotos } from "./projects/VHSDreams";
+
+// EVERSTRIDE:
+import {
+  Everstride,
+  everstrideLink,
+  everstridePhotos,
+} from "./projects/Everstride";
 
 function App() {
   // Function to scroll to <Article> FROM <Navbar>
@@ -47,16 +56,20 @@ function App() {
       <div className="Article-ref" ref={articleRef}>
         <Article />
       </div>
+
+      {/* EVERSTRIDE */}
       <br className="break" />
-      <ParallaxText text="PHOTOLABS" backwards={false} speed={3} />
-      <ParallaxText text="PHOTOLABS" backwards={true} speed={3} />
+      <ParallaxText text="EVERSTRIDE" backwards={false} speed={3} />
+      <ParallaxText text="EVERSTRIDE" backwards={true} speed={3} />
       <br className="break" />
       <Project
-        description={<PhotoLabs />}
-        link={photoLabsLink}
-        photos={photoLabsPhotos}
-        title="PhotoLabs"
+        description={<Everstride />}
+        link={everstrideLink}
+        photos={everstridePhotos}
+        title="Everstride"
       />
+
+      {/* VHS DREAMS */}
       <br className="break" />
       <ParallaxText text="VHS DREAMS" backwards={false} speed={3} />
       <ParallaxText text="VHS DREAMS" backwards={true} speed={3} />
@@ -67,6 +80,20 @@ function App() {
         photos={vhsPhotos}
         title="VHS Dreams"
       />
+
+      {/* PHOTOLABS */}
+      <br className="break" />
+      <ParallaxText text="PHOTOLABS" backwards={false} speed={3} />
+      <ParallaxText text="PHOTOLABS" backwards={true} speed={3} />
+      <br className="break" />
+      <Project
+        description={<PhotoLabs />}
+        link={photoLabsLink}
+        photos={photoLabsPhotos}
+        title="PhotoLabs"
+      />
+
+      {/* TWEETER */}
       <br className="break" />
       <ParallaxText text="TWEETER" backwards={false} speed={3} />
       <ParallaxText text="TWEETER" backwards={true} speed={3} />
@@ -77,6 +104,7 @@ function App() {
         photos={tweeterPhotos}
         title="Tweeter"
       />
+
       <Footer />
     </div>
   );
---
+++ src/projects/Everstride.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+export const everstrideLink = "https://github.com/kylemcparland/everstride";
+
+export const everstridePhotos = [
+  {
+    url: "images/Everstride/1E-add-progress.gif",
+    caption:
+      "No Strava APP? No problem! Manually add your steps to progress on your quest!",
+  },
+  {
+    url: "images/Everstride/8E-strava.png",
+    caption:
+      "With your Strava account connected, simply visit the page and see how far you've come!",
+  },
+  {
+    url: "images/Everstride/2E-avatar.png",
+    caption: "Customize your avatar to stand out from your friends!",
+  },
+  {
+    url: "images/Everstride/3E-complete-quest.gif",
+    caption:
+      "Completing a quest leads to a choice - and the roll of a dice! Make your choice wisely!",
+  },
+  {
+    url: "images/Everstride/4E-map.png",
+    caption:
+      "The world map tracks your overall progress! Your location is reflected in your backdrop!",
+  },
+  {
+    url: "images/Everstride/5E-add-friend.gif",
+    caption:
+      "Get social: add friends! No scrolling required - the page will change dynamically!",
+  },
+  {
+    url: "images/Everstride/6E-store.png",
+    caption:
+      "Purchase new equipment using your hard-earned gold gained from quests!",
+  },
+  {
+    url: "images/Everstride/7E-stripe.png",
+    caption:
+      "Or make a secure payment with Stripe to earn gold the old fashioned way!",
+  },
+];
+
+export const Everstride = () => {
+  return (
+    <div>
+      <span>
+        Next.js &#x2022; PostgreSQL &#x2022; Strava API &#x2022; Stripe API
+      </span>
+      <p>
+        <i>Everstride</i> is a single-page application that gamifies the user's
+        exercise data and translates it into a social,{" "}
+        <strong>DnD-style adventure</strong>. Utilizing the{" "}
+        <a
+          href="https://developers.strava.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Strava API
+        </a>
+        , the user's <strong>daily step goals</strong> become dynamic quests
+        that can earn them gold to buy equipment to customize their in-game{" "}
+        <strong>Avatar</strong>. Additionally, the{" "}
+        <a
+          href="https://docs.stripe.com/api"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Stripe API
+        </a>{" "}
+        allows users to skip the run and earn their gold through secure
+        payments.
+        <br />
+        <br />
+        This project was a culmination of everything I've come to know and love
+        about web development. I dove head-first into an exciting new framework
+        with <strong>Next.js</strong> and explored powerful APIs like{" "}
+        <strong>Strava</strong> & <strong>Stripe</strong>. I utilized both
+        client-side and server-side functionality to create an engaging
+        front-end that responds dynamically to the user. I crafted a robust
+        back-end that handled data efficiently while maintaining a DRY and
+        modular design for scalability. Finally, I built it all on top of a
+        normalized database that made the most out of my <strong>SQL</strong> &{" "}
+        <strong>ERD</strong> abilities.
+      </p>
+    </div>
+  );
+};
